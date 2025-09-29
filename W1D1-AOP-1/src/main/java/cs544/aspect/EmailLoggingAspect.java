1c6e1dfdc486c61a134d7cee5e7f6a7b72c610cb
quynhdv <quynhvdinh@gmail.com>
2025-09-29 13:52:31 -0500

    JointPoint argument

--- W1D1-AOP-1/src/main/java/cs544/aspect/EmailLoggingAspect.java
+++ W1D1-AOP-1/src/main/java/cs544/aspect/EmailLoggingAspect.java
@@ -1,14 +1,18 @@
 package cs544.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import cs544.EmailSender;
 
 @Aspect
 @Component
 public class EmailLoggingAspect {
     @After("execution(* cs544.EmailSender.sendEmail(..)) && args(email, message)")
-    public void logEmailSent(String email, String message) {
-        System.out.println(new java.util.Date() + " method= sendEmail address=" + email + " message=" + message);
+    public void logEmailSent(JoinPoint joinPoint, String email, String message) {
+        EmailSender emailSender = (EmailSender) joinPoint.getTarget();
+        String outgoingMailServer = emailSender.getOutgoingMailServer();
+        System.out.println(new java.util.Date() + " method= sendEmail address=" + email + " message= " + message + " outgoing mail server = " + outgoingMailServer);
     }
 }
