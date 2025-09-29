657261ba35a3949e592e44ee04aaca5532a358db
quynhdv <quynhvdinh@gmail.com>
2025-09-29 13:27:14 -0500

    w1d1

---
+++ W1D1-AOP-1/src/main/java/cs544/App.java
@@ -0,0 +1,26 @@
+/*​​​​​​​​​​​​​​​​​​​​​‌‌​​‌​​‌​​​ See LICENSE.md for the source code license */
+
+package cs544;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+	@Autowired
+	private ICustomerService customerService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		customerService.addCustomer("Frank Brown", "fbrown@acme.com",
+				"mainstreet 5", "Chicago", "60613");
+	}
+
+}
