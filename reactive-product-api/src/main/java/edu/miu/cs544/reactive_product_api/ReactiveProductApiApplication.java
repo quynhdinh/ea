17bdcd18e0189462bed5c4153f3909cc8f477eed
quynhdv <quynhvdinh@gmail.com>
2025-10-01 13:28:06 -0500

    init reactive-product-api

---
+++ reactive-product-api/src/main/java/edu/miu/cs544/reactive_product_api/ReactiveProductApiApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.reactive_product_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveProductApiApplication.class, args);
+	}
+
+}
---
+++ reactive-product-api/src/test/java/edu/miu/cs544/reactive_product_api/ReactiveProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.reactive_product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
