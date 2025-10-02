b2a256d67653d518a96fc34f95cb90ad75a82fce
quynhdv <quynhvdinh@gmail.com>
2025-10-02 14:03:06 -0500

    add graphql things

--- spring-websocket-postgresql/src/main/java/edu/miu/cs544/spring_websocket_postgresql/App.java
+++ spring-websocket-postgresql/src/main/java/edu/miu/cs544/spring_websocket_postgresql/App.java
@@ -3,6 +3,10 @@ package edu.miu.cs544.spring_websocket_postgresql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.*;
@@ -11,6 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.OffsetDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class App {
@@ -37,7 +42,6 @@ class Product {
 @Getter
 @Entity
 @Table(name = "messages")
-@NoArgsConstructor // Default constructor required by JPA
 class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -57,6 +61,9 @@ class Message {
         this.content = content;
         this.timestamp = OffsetDateTime.now();
     }
+	public Message() {
+		// default constructor
+	}
 
     @Override
     public String toString() {
@@ -66,3 +73,29 @@ class Message {
 
 @Repository
 interface MessageRepository extends JpaRepository<Message, Long> {}
+
+@Controller
+class MessageController {
+    private final MessageRepository messageRepository;
+
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    // Define GraphQL queries and mutations here
+	@QueryMapping
+	public List<Message> messages() {
+		return messageRepository.findAll();
+	}
+
+	@QueryMapping
+	public Message messageById(@Argument Long id) {
+		return messageRepository.findById(id).orElse(null);
+	}
+
+	@MutationMapping
+	public Message createMessage(@Argument String content, @Argument String sender) {
+		Message message = new Message(sender, content);
+		return messageRepository.save(message);
+	}
+}
